9bbbb29f64016fab57db699b67807c4d6b8f527e
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-03-21 22:32:26 +0200

    Header comp. updated with useScroll Hook

--- src/components/UI/Header/Header.js
+++ src/components/UI/Header/Header.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './Header.css';
+import logo from '../../../images/EHSH.png';
+import useScroll from '../../../components/customHooks/useScroll';
+import EjectIcon from '@material-ui/icons/Eject';
 
 const Header = () => {
+  const { x, y } = useScroll();
+
   return (
     <div className='header'>
       <div className='logo'>
-        <span class='logo-line'></span>
-        <span class='logo-line'></span>
+        <img src={logo} title='Entertainment Hub' alt='Entertainment Hub' />
       </div>
       <h1>
         <span>E</span>ntertainment <span>H</span>
         <span style={{ color: 'red', fontSize: '1.3rem' }}>ub</span>
       </h1>
+      {y >= 400 ? (
+        <div className='header-buttons'>
+          <span
+            onClick={() => window.scroll({ top: 0, behavior: 'smooth' })}
+            className='scroll-up'
+            title='Scroll Up'
+          >
+            <EjectIcon style={{ color: 'red', fontSize: '30px' }} />
+          </span>
+        </div>
+      ) : null}
     </div>
   );
 };
