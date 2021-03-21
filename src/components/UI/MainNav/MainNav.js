16826379cafbaa280ca509e5a90d4eef0ae6501b
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-03-21 22:32:54 +0200

    bottom Main Nav comp Created

--- src/components/UI/MainNav/MainNav.js
+++ src/components/UI/MainNav/MainNav.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
-
+import { useHistory } from 'react-router-dom';
 import MovieIcon from '@material-ui/icons/Movie';
 import WhatshotIcon from '@material-ui/icons/Whatshot';
 import TvIcon from '@material-ui/icons/Tv';
@@ -10,7 +10,7 @@ import YoutubeSearchedForIcon from '@material-ui/icons/YoutubeSearchedFor';
 
 const useStyles = makeStyles({
   root: {
-    width: '100%',
+    width: '100vw',
     background: '#2e3542',
     position: 'fixed',
     bottom: 0,
@@ -21,7 +21,30 @@ const useStyles = makeStyles({
 
 const MainNav = () => {
   const classes = useStyles();
-  const [value, setValue] = useState(0);
+  const history = useHistory();
+  const [value, setValue] = useState();
+
+  useEffect(() => {
+    if (history) {
+      switch (value) {
+        case 0:
+          history.push('/trending');
+          break;
+        case 1:
+          history.push('/movies');
+          break;
+        case 2:
+          history.push('/series');
+          break;
+        case 3:
+          history.push('/search');
+          break;
+        default:
+          history.push('/');
+          break;
+      }
+    }
+  }, [value, history]);
 
   return (
     <BottomNavigation
@@ -32,12 +55,33 @@ const MainNav = () => {
       showLabels
       className={classes.root}
     >
-      <BottomNavigationAction label='Trending' icon={<WhatshotIcon />} />
-      <BottomNavigationAction label='Movies' icon={<MovieIcon />} />
-      <BottomNavigationAction label='TV Series' icon={<TvIcon />} />
+      <BottomNavigationAction
+        label='Trending'
+        icon={<WhatshotIcon />}
+        style={{
+          color: value === 0 ? 'rgb(253, 253, 61)' : 'rgba(255, 255, 255, 0.3)',
+        }}
+      />
+      <BottomNavigationAction
+        label='Movies'
+        icon={<MovieIcon />}
+        style={{
+          color: value === 1 ? 'rgb(253, 253, 61)' : 'rgba(255, 255, 255, 0.3)',
+        }}
+      />
+      <BottomNavigationAction
+        label='Series'
+        icon={<TvIcon />}
+        style={{
+          color: value === 2 ? 'rgb(253, 253, 61)' : 'rgba(255, 255, 255, 0.3)',
+        }}
+      />
       <BottomNavigationAction
         label='Search'
         icon={<YoutubeSearchedForIcon />}
+        style={{
+          color: value === 3 ? 'rgb(253, 253, 61)' : 'rgba(255, 255, 255, 0.3)',
+        }}
       />
     </BottomNavigation>
   );
