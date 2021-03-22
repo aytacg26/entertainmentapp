276c8519897f58df17f1b1c4b6605cc80839b836
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-03-23 01:58:45 +0200

    New Lang. and a New Helper Method Added

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -6,48 +6,122 @@ export const getLanguage = (code) => {
       return 'Turkish';
     case 'az':
       return 'Azerbaijani';
+    case 'ar':
+      return 'Arabic';
     case 'es':
       return 'Spanish';
     case 'ja':
       return 'Japanese';
-    case 'fr':
-      return 'French';
+    case 'cs':
+      return 'Czech';
+    case 'bg':
+      return 'Bulgarian';
+    case 'bs':
+      return 'Bosnian';
+    case 'be':
+      return 'Belarusian';
     case 'et':
       return 'Estonian';
     case 'fi':
       return 'Finnish';
+    case 'fr':
+      return 'French';
+    case 'he':
+      return 'Hebrew';
+    case 'hu':
+      return 'Hungarian';
     case 'de':
       return 'German';
     case 'el':
       return 'Greek';
+    case 'is':
+      return 'Icelandic';
+    case 'it':
+      return 'Italian';
+    case 'id':
+      return 'Indonesian';
     case 'hi':
       return 'Hindi';
+    case 'mn':
+      return 'Mongolian';
+    case 'mk':
+      return 'Macedonian';
     case 'no':
       return 'Norwegian';
+    case 'ne':
+      return 'Nepali';
     case 'ru':
       return 'Russian';
     case 'th':
       return 'Thai';
+    case 'tk':
+      return 'Turkmen';
     case 'uk':
       return 'Ukrainian';
     case 'fa':
       return 'Persian';
     case 'ko':
       return 'Korean';
-    case 'it':
-      return 'Italian';
     case 'ga':
       return 'Irish';
+    case 'pa':
+      return 'Panjabi';
     case 'pl':
       return 'Polish';
+    case 'pt':
+      return 'Portuguese';
+    case 'ka':
+      return 'Georgian';
+    case 'ku':
+      return 'Kurdish';
+    case 'ky':
+      return 'Kyrgyz';
+    case 'kk':
+      return 'Kazakh';
+    case 'lv':
+      return 'Latvian';
+    case 'lt':
+      return 'Lithuanian';
     case 'ro':
       return 'Romanian';
     case 'sr':
       return 'Serbian';
+    case 'sv':
+      return 'Swedish';
+    case 'sk':
+      return 'Slovak';
     case 'hy':
       return 'Armenian';
+    case 'ur':
+      return 'Urdu';
+    case 'uz':
+      return 'Uzbek';
+    case 'vi':
+      return 'Vietnamese';
+    case 'yi':
+      return 'Yiddish';
+    case 'zh':
+      return 'Chinese';
 
     default:
       return code;
   }
 };
+
+export const createDates = (start) => {
+  const today = new Date();
+  const currentYear = parseInt(today.getFullYear());
+  const dates = [];
+
+  for (let i = start; i < currentYear + 1; i++) {
+    const datesObj = {};
+    datesObj.id = `year-${i}`;
+    datesObj.value = parseInt(i);
+    datesObj.text = i.toString();
+    dates.push(datesObj);
+  }
+
+  return dates;
+};
+
+export const getLanguages = () => {};
