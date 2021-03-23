3b81364f2cd9f0c7ec80aabaaa12de5332830dcd
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-03-23 22:47:47 +0200

    useLocal custom hook planned

---
+++ src/customHooks/useLocal.js
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from 'react';
+
+const useLocal = (name, value) => {
+  //arg will be initial argument and it can be string, number, object or array
+  //we will need a func, which will set data value
+  //for localStorage we have setItem and getItem
+};
