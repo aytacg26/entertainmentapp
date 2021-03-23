48f4b7140eb8f2cfa039bceb15cc884cc8e7686e
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-03-23 19:10:40 +0200

    pagination removed and fetch by scroll added

--- src/components/Pages/Trending/Trending.js
+++ src/components/Pages/Trending/Trending.js
@@ -1,115 +1,96 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Card from '../../Card/Card';
 import axios from 'axios';
 import './trending.css';
 import Loader from '../../UI/Loader/Loader';
 import PageTitleContainer from '../../UI/PageTitleContainer/PageTitleContainer';
-import Pagination from '../../UI/Pagination/Pagination';
 import MediaTypes from '../../UI/MediaTypes/MediaTypes';
+import useScroll from '../../../customHooks/useScroll';
 
 //Use Redux for Types, Pagination, Filtering (For filtering, instead of current page, we need to filter from 1000x20 trending media)
-const Trending = (props) => {
+const Trending = () => {
   const [trendingMovies, setTrendingMovies] = useState(null);
   const [loading, setLoading] = useState(true);
-  const [search, setSearch] = useState('');
-  const [isMounted, setIsMounted] = useState(true);
   const [page, setPage] = useState(1);
-  const [totalPages, setTotalPages] = useState(0);
+  const [fetchedPages, setFetchedPages] = useState([]);
   const [type, setType] = useState('all');
+  const target = useRef();
+  const [scrollData, screenHeight, pageScrollHeight] = useScroll(target);
 
+  //It will be called every page change but page change will be done according to scroll and new data will be added to current data
   const fetchTrending = async () => {
-    let numToUse = 100;
     const { data } = await axios.get(`
-    https://api.themoviedb.org/3/trending/${type}/week?api_key=${process.env.REACT_APP_API_KEY}&page=${page}&total_pages`);
+      https://api.themoviedb.org/3/trending/${type}/week?api_key=${process.env.REACT_APP_API_KEY}&page=${page}&total_pages`);
 
-    setTrendingMovies(data.results);
-    setLoading(false);
+    setFetchedPages([...fetchedPages.filter((fp) => fp !== page), page]);
 
-    if (data.total_pages < 100) {
-      numToUse = data.total_pages;
+    if (trendingMovies) {
+      setTrendingMovies([...trendingMovies, ...data.results]);
+      setLoading(false);
+    } else {
+      setTrendingMovies(data.results);
+      setLoading(false);
     }
-
-    setTotalPages(numToUse);
   };
 
   useEffect(() => {
-    if (isMounted) {
+    if (!fetchedPages.includes(page)) {
       fetchTrending();
     }
-    return () => {
-      setIsMounted(false);
-    };
+
     //eslint-disable-next-line
-  }, [isMounted, page]);
+  }, [page]);
 
-  const movies =
-    trendingMovies !== null
-      ? trendingMovies.filter((movie) => {
-          const movieName = movie.title || movie.name;
-          const movieType = movie.media_type === 'tv' ? 'tv series' : 'movie';
+  useEffect(() => {
+    const pageNum = Math.ceil(
+      (scrollData.y + (screenHeight - 300)) / pageScrollHeight
+    );
+    console.log(pageNum);
+    if (
+      !fetchedPages.includes(pageNum) &&
+      trendingMovies &&
+      pageNum > 0 &&
+      pageNum <= 5
+    ) {
+      setPage(pageNum);
+    }
 
-          return (
-            movieName.toLowerCase().includes(search.toLowerCase()) ||
-            movieType.toLowerCase().includes(search.toLowerCase())
-          );
-        })
-      : trendingMovies;
-
-  //   return (
-  //     <div className='page-container'>
-  //       <div className='page-title-area'>
-  //         <span className='page-title'>Trending</span>
-  //         <div className='filter-area'>
-  //           <input
-  //             type='search'
-  //             name='search'
-  //             className='page-search-box'
-  //             placeholder='Filter Movies...'
-  //             autocomplete='off'
-  //             onChange={(e) => setSearch(e.target.value)}
-  //             value={search}
-  //           />
-  //         </div>
-  //       </div>
-  //       <div className='trending'>
-  //         {trendingMovies === null && loading ? (
-  //           <Loader />
-  //         ) : trendingMovies.length === 0 ? (
-  //           <span>No Tranding Movie</span>
-  //         ) : (
-  //           movies.map((movie) => <Card key={movie.id} movie={movie} />)
-  //         )}
-  //       </div>
-  //     </div>
-  //   );
-  const handlePage = (pageNum) => {
-    setPage(parseInt(pageNum));
-    setIsMounted(true);
-    window.scroll(0, 0);
-  };
+    //eslint-disable-next-line
+  }, [scrollData.y]);
 
   const handleType = (data) => {
+    console.log(data);
     setType(data.type);
+    setFetchedPages([1]);
     setPage(1);
-    setIsMounted(true);
     window.scroll(0, 0);
   };
+
+  //Some of the move comes 2 times from server because they are trending in different weeks or there is something wrong in API
+  //we need to get distict data from all and present them (when we are updating the state, we have to compare previous data with new data and create a distict movies data)
+  const movies =
+    type === 'all'
+      ? trendingMovies
+      : trendingMovies.filter((movie) => movie.media_type === type);
 
   return (
     <PageTitleContainer title='Tranding'>
       <MediaTypes getType={handleType} />
-      <div className='page-cards-section'>
+      <div className='page-cards-section' ref={target}>
         {trendingMovies === null && loading ? (
           <Loader />
         ) : trendingMovies.length === 0 ? (
           <span>No Tranding Movie</span>
         ) : (
-          movies.map((movie) => (
-            <Card key={movie.id} movie={movie} showType={true} />
+          movies.map((movie, index) => (
+            <Card
+              key={`${movie.id}-${movie.media_type}-${index}`}
+              movie={movie}
+              showType={true}
+            />
           ))
         )}
       </div>
-      <Pagination onChange={handlePage} count={totalPages} page={page} />
     </PageTitleContainer>
   );
 };
