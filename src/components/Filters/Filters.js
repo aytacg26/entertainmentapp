7cc7984be88393b73adadee4386d312836a5a3b7
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-03-23 22:34:42 +0200

    New Filters Added

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -1,20 +1,79 @@
-import React, { useState, useEffect } from 'react';
-import { createDates } from '../../utils/helper';
+import React, { useState } from 'react';
+
 import DropDown from '../UI/DropDown/DropDown';
+import Checkbox from '../UI/CheckBox/CheckBox';
+import useFilter from '../../customHooks/useFilter';
+import './filters.css';
 
 const Filters = (props) => {
-  const [dates, setDates] = useState([]);
+  const [dates, languages, sort, points] = useFilter();
+  const [check, setCheck] = useState(false);
 
-  useEffect(() => {
-    const datesArr = createDates(1890);
+  const handleChange = (e) => {
+    const name = e.target.name;
 
-    setDates(datesArr);
-  }, []);
+    switch (name) {
+      case 'date':
+        console.log(e.target.value);
+        break;
+      case 'point':
+        console.log(e.target.value);
+        break;
+      case 'language':
+        console.log(e.target.value);
+        break;
+      case 'sortBy':
+        console.log(e.target.value);
+        break;
+      case 'adult':
+        setCheck(!check);
+        break;
+      default:
+        return null;
+    }
+  };
 
   return (
-    <div>
-      <DropDown options={dates} title='Movie Year' />
-      <DropDown options={dates} title='Language' />
+    <div className='filter-options'>
+      <DropDown
+        options={dates}
+        valueKey='value'
+        textKey='text'
+        title='Movie Year'
+        name='date'
+        onChange={handleChange}
+      />
+      <DropDown
+        options={languages}
+        valueKey='code'
+        textKey='name'
+        title='Language'
+        name='language'
+        onChange={handleChange}
+      />
+      <DropDown
+        options={sort}
+        valueKey='value'
+        textKey='name'
+        title='Sort By'
+        name='sortBy'
+        onChange={handleChange}
+      />
+      <DropDown
+        options={points}
+        valueKey='value'
+        textKey='name'
+        title='Point'
+        name='point'
+        onChange={handleChange}
+      />
+      <Checkbox
+        title='Include Adult'
+        name='adult'
+        value={check}
+        checked={check}
+        onChange={handleChange}
+      />
     </div>
   );
 };
