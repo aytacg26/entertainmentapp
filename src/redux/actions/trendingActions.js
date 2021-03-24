31c3789dcbd5bb28068faf28382d2c6212416ed0
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-03-24 22:39:57 +0200

    Redux trending actions created

---
+++ src/redux/actions/trendingActions.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+import {
+  SET_TRENDING,
+  SET_PAGE_NUMBER,
+  SET_MOVIE_TYPE,
+  SET_LOADING,
+  RESET_FETCHED_PAGES,
+  FETCH_ERROR,
+} from './types';
+
+export const resetFetchedPages = () => ({
+  type: RESET_FETCHED_PAGES,
+});
+
+export const setPageNumber = (page) => ({
+  type: SET_PAGE_NUMBER,
+  payload: page,
+});
+
+export const setLoading = () => ({
+  type: SET_LOADING,
+});
+
+export const fetchTrending = (page, type) => {
+  //   addToFetchedPages();
+  return async (dispatch) => {
+    try {
+      setLoading();
+
+      const { data } = await axios.get(
+        `https://api.themoviedb.org/3/trending/${type}/week?api_key=${process.env.REACT_APP_API_KEY}&page=${page}&total_pages`
+      );
+
+      dispatch({ type: SET_TRENDING, payload: data.results });
+    } catch (error) {
+      console.error(error);
+      dispatch({ type: FETCH_ERROR, payload: error.response });
+    }
+  };
+};
