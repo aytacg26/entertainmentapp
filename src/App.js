54320591bfbbaab7f45ec7e041491368db312e3e
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-03-26 00:23:41 +0200

    lazy loading added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, lazy, Suspense } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './components/UI/Header/Header';
@@ -6,11 +6,14 @@ import MainNav from './components/UI/MainNav/MainNav';
 import { Container } from '@material-ui/core';
 import Home from './components/Pages/Home/Home';
 import Trending from './components/Pages/Trending/Trending';
-import Movies from './components/Pages/Movies/Movies';
+// import Movies from './components/Pages/Movies/Movies';
 import Series from './components/Pages/Series/Series';
 import Search from './components/Pages/Search/Search';
+import Loader from './components/UI/Loader/Loader';
 import store from './redux/store';
 import { Provider } from 'react-redux';
+
+const Movies = lazy(() => import('./components/Pages/Movies/Movies'));
 
 const App = () => {
   return (
@@ -23,12 +26,20 @@ const App = () => {
               <Switch>
                 <Route path='/' component={Home} exact />
                 <Route path='/trending' component={Trending} />
-                <Route path='/movies' component={Movies} />
+                <Route
+                  path='/movies'
+                  render={() => (
+                    <Suspense fallback={<Loader />}>
+                      <Movies />
+                    </Suspense>
+                  )}
+                />
                 <Route path='/series' component={Series} />
                 <Route path='/search' component={Search} />
               </Switch>
             </Container>
           </div>
+
           <MainNav />
         </Fragment>
       </Router>
