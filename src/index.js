2663eb03a24ceda50692eb7a82a3555faf7ffb24
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-03-27 22:13:24 +0200

    Redux Provider added

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import store from './redux/store';
+import { Provider } from 'react-redux';
+
+const app = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
 
 ReactDOM.render(
-  <React.StrictMode>
-    {' '}
-    <App />
-  </React.StrictMode>,
+  <React.StrictMode> {app}</React.StrictMode>,
   document.getElementById('root')
 );
