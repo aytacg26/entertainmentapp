f64925f797f607a90672d1a3ff38bd2ab8ae4304
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-03-27 22:16:43 +0200

    trending Redux actions completely updated

--- src/redux/actions/trendingActions.js
+++ src/redux/actions/trendingActions.js
@@ -1,40 +1,77 @@
 import axios from 'axios';
+import { setLoading, removeLoading, setMessage } from './utilActions';
+
 import {
   SET_TRENDING,
-  SET_PAGE_NUMBER,
   SET_MOVIE_TYPE,
-  SET_LOADING,
-  RESET_FETCHED_PAGES,
-  FETCH_ERROR,
+  SET_BOTTOM,
+  REMOVE_BOTTOM,
 } from './types';
 
-export const resetFetchedPages = () => ({
-  type: RESET_FETCHED_PAGES,
-});
-
-export const setPageNumber = (page) => ({
-  type: SET_PAGE_NUMBER,
-  payload: page,
-});
-
-export const setLoading = () => ({
-  type: SET_LOADING,
-});
-
 export const fetchTrending = (page, type) => {
-  //   addToFetchedPages();
   return async (dispatch) => {
     try {
-      setLoading();
-
       const { data } = await axios.get(
         `https://api.themoviedb.org/3/trending/${type}/week?api_key=${process.env.REACT_APP_API_KEY}&page=${page}&total_pages`
       );
 
-      dispatch({ type: SET_TRENDING, payload: { data: data.results, page } });
+      if (data) {
+        const newPage = page + 1;
+
+        dispatch({
+          type: SET_TRENDING,
+          payload: { data: data.results, page: newPage },
+        });
+        dispatch(removeLoading());
+      }
     } catch (error) {
-      console.error(error);
-      dispatch({ type: FETCH_ERROR, payload: error.response });
+      console.log(error.message);
+      if (!window.navigator.onLine) {
+        dispatch(
+          setMessage(
+            'Please check your internet connection',
+            'error',
+            'No internet connection'
+          )
+        );
+      } else {
+        dispatch(
+          setMessage(
+            'An unexpected server error occured, we are sorry...',
+            'error',
+            'Unexpected Server Error'
+          )
+        );
+      }
     }
+  };
+};
+
+export const handleScroll = (element) => (dispatch) => {
+  const browserHeight = window.innerHeight;
+  const scrollPosition = window.pageYOffset;
+  const elementHeight = element.current.clientHeight;
+  const elementIndent = element.current.offsetTop;
+  const childHeight = element.current.firstElementChild?.clientHeight;
+
+  if (
+    browserHeight + scrollPosition + 2 * childHeight >=
+    elementHeight + elementIndent
+  ) {
+    console.log('Dispatching Loading....');
+    dispatch(setLoading());
+    console.log('Dispatching SET BOTTOM...');
+    dispatch({ type: SET_BOTTOM });
+  }
+};
+
+export const removeBottom = () => ({
+  type: REMOVE_BOTTOM,
+});
+
+export const setType = (type) => {
+  return {
+    type: SET_MOVIE_TYPE,
+    payload: type,
   };
 };
