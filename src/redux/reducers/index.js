0a16d06210555b9a677a523d015644e16f3a709a
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-03-27 22:18:45 +0200

    util reducer added to rootReducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 import trendingReducer from './trendingReducer';
+import utilReducer from './utilReducer';
 
 //we will add our reducers to combineReducers. This is our rootReducer
 export default combineReducers({
   trending: trendingReducer,
+  message: utilReducer,
 });
